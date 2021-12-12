fe5eb049685c8fe6bf78a5582b7c49b1271d8b6d
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-12 17:02:33 +0000

    Counter set up, bugs to be addressed, internal server error when url is visited

--- src/lambda.js
+++ src/lambda.js
@@ -1,10 +1,33 @@
 import AWS from "aws-sdk";
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
+// Visit function
 export async function handler(event) {
+  // get visitor name
+  const name = event.queryStringParameters.name;
+
+  // get current count
+  const results = await dynamoDb.get({
+    TableName: process.env.tableName,
+    Key: { visitor: name },
+  }).promise();
+
+  let count = results.Item ? results.Item.tally : 0;
+  count++;
+
+  // Set new count
+  await dynamoDb.update({
+    TableName: process.env.tableName,
+    Key: { visitor: name },
+    UpdateExpression: "SET tally = :count",
+    ExpressionAttributeValues: {
+      ":count": count
+    }
+  }).promise();
+
   return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `My serverless app is now running in dev mode. CHANGE MADE! Request received at ${event.requestContext.time}.`,
-  };
+    statuscode: 200,
+    body: `Hey, ${name}, you have visited ${count} times.`,
+  }
+
 }
