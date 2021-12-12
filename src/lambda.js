79d293d77aa098f89125ac8623e0c051724119ab
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-12 16:31:36 +0000

    API endpoint established, showing a simple text message. DB set up

--- src/lambda.js
+++ src/lambda.js
@@ -1,7 +1,10 @@
+import AWS from "aws-sdk";
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
 export async function handler(event) {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: `Hello, World! Your request was received at ${event.requestContext.time}.`,
+    body: `My serverless app is now running in dev mode. CHANGE MADE! Request received at ${event.requestContext.time}.`,
   };
 }
--- stacks/MyStack.js
+++ stacks/MyStack.js
@@ -4,12 +4,27 @@ export default class MyStack extends sst.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
+    // Create the table in the DB
+    const table = new sst.Table(this, "Counter", {
+      fields: {
+        visitor: sst.TableFieldType.STRING,
+      },
+      primaryIndex: { partitionKey: "visitor" }
+    });
+
     // Create a HTTP API
     const api = new sst.Api(this, "Api", {
       routes: {
         "GET /": "src/lambda.handler",
+        "GET /visit": "src/lambda.handler",
       },
+      defaultFunctionProps: {
+        environment: {
+          tableName: table.dynamodbTable.tableName,
+        }
+      }
     });
+    api.attachPermissions([table])
 
     // Show the endpoint in the output
     this.addOutputs({
